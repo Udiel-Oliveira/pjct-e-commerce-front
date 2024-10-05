defcd91b94e8f29cf252d07e161d8ef6d562345a
Udiel-Oliveira <udiel.inaciosilva@gmail.com>
2024-10-05 19:02:50 -0300

    feat: ajustando a navbar colocando o active da bolinha

--- gentix-store/src/app/components/layout/Navbar.js
+++ gentix-store/src/app/components/layout/Navbar.js
@@ -1,53 +1,64 @@
-import Link from "next/link"
-import styles from './navbar.module.css'
+"use client"
+
+// Navbar.js
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+import styles from './navbar.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faWallet } from '@fortawesome/free-solid-svg-icons';
-export default function NavBar(){
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
-    return(
-        <nav className={styles.nav}>
-            
+export default function NavBar() {
+  const currentRoute = usePathname(); // Obter a rota atual com usePathname
 
-            <ul className={styles.list}>
+  return (
+    <nav className={styles.nav}>
+      <ul className={styles.list}>
+        <Link href="/SignIn" className={styles.logo}>
+          <img src="/assets/logoHome.png" alt="" />
+        </Link>
 
-            <Link href="./SignIn" className={styles.logo}>
-                <img src="/assets/logoHome.png" alt="" />
-            </Link>
+        <li className={styles.link}>
+          <Link href="/" className={styles.linkItem}>
+          <div className={styles.iconConteiner}>
+            <img src="/assets/Xbox_Icon.svg" />
+            {(currentRoute === '/' || currentRoute === '/') && (
+              <span className={styles.activeDot}></span>
+            )}
+          </div>
+            Jogos
+          </Link>
+        </li>
 
-        
-            <li className={styles.link}>
-                <Link href={"./pages/SignUp"} className={styles.linkItem}>
-                    <img src="/assets/Xbox_Icon.svg"/>
-                    Jogos
-                </Link>
-            </li>
+        <div className={styles.searchInput}>
+          <img src="/assets/lupa.svg" alt="" />
+          <input type="search" placeholder="Pesquisar..." />
+        </div>
+      </ul>
 
-            <div className={styles.searchInput}>
-                <img src="/assets/lupa.svg" alt=""/>
-                <input type="search"
-                placeholder="Pesquisar..." 
-                />
+      <div className={styles.user}>
+        <div>
+          <Link href="/pages/Store" className={styles.linkItem}>
+            <div className={styles.iconConteiner}>
+                <img src="/assets/Store_Icon.svg" alt="" />
+                {currentRoute === '/pages/Store' && <span className={styles.activeDot}></span>}
             </div>
+            Loja
+          </Link>
+        </div>
+        <div>
+          <Link href="/pages/Biblioteca" className={styles.linkItem}>
+            <div className={styles.iconConteiner}>
+                <img src="/assets/Biblioteca_Icon.svg" alt="" />
+                {currentRoute === '/pages/Biblioteca' && <span className={styles.activeDot}></span>}
+            </div>
+            Biblioteca
+          </Link>
+        </div>
 
-            </ul>
-    
-            <div className={styles.user}>
-            <div>
-               <Link href={"./pages/SignUp"} className={styles.linkItem}>
-                    <img src="/assets/Store_Icon.svg" alt="" />
-                    Loja
-               </Link> 
-            </div>
-            <div>
-               <Link href={"./pages/SignUp"} className={styles.linkItem}>
-                    <img src="/assets/Biblioteca_Icon.svg" alt="" />
-                    Biblioteca
-               </Link> 
-            </div>
-
-                <Link className={styles.userImage}  href={"./pages/SignUp"}> <FontAwesomeIcon icon={faUser} style={{ width: '23', height: '23' }} /></Link>    
-            </div>
-            
-        </nav>
-    )
+        <Link className={styles.userImage} href="/pages/SignUp">
+          <FontAwesomeIcon icon={faUser} style={{ width: '23px', height: '23px' }} />
+        </Link>
+      </div>
+    </nav>
+  );
 }
