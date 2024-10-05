616e713435288587376d23d25ac479cce4d10c02
Udiel-Oliveira <udiel.inaciosilva@gmail.com>
2024-10-05 19:34:13 -0300

    fix: problema de build

--- gentix-store/src/app/pages/Biblioteca/page.js
+++ gentix-store/src/app/pages/Biblioteca/page.js
@@ -1,5 +1,6 @@
 import NavBar from "@/app/components/layout/Navbar";
 import styles from "../Biblioteca/biblioteca.module.css"
+import Head from "next/head";
 //seu gay, a nav ta ali so pra visualização 
 
 export default function Home() {
