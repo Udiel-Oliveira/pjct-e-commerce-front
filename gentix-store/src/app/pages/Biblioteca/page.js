baad82a9bcdd19dcc95500b7f493c7c7ecb8e7e3
Udiel-Oliveira <udiel.inaciosilva@gmail.com>
2024-11-22 15:01:47 -0300

    fix: erro de build

--- gentix-store/src/app/pages/Biblioteca/page.js
+++ gentix-store/src/app/pages/Biblioteca/page.js
@@ -1,15 +1,16 @@
 import NavBar from "@/app/components/layout/NavBar/Navbar";
 import styles from "../Biblioteca/biblioteca.module.css"
-import Footer from "@/app/components/layout/Footer";
+import Footer from "@/app/components/layout/Footer/Footer";
+
 
 export default function Home() {
   return (
     <div>
-    <NavBar/>
+      <NavBar/>
       <div className={styles.page}>
         <h4  className={styles.title}>Meus jogos</h4>
       </div>
-     <Footer/>
+    <Footer/>
     </div>
 
     
